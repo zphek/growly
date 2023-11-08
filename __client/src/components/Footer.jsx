82aa0e306120e97fe5a4a95e722ff69679b06ee3
bernardobaez <bernardbaez.beno@gmail.com>
2023-11-08 00:34:55 -0400

    ...

---
+++ __client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (state.show && (
+        <div>
+
+        </div>
+    ));
+}
+ 
+export default Footer;
---
+++ __client/src/contexts/GeneralProvider.jsx
@@ -0,0 +1,45 @@
+import { createContext, useReducer } from "react";
+
+const GeneralContext = createContext();
+
+function Reducer(state, action){
+    switch(action.type){
+        case "toggle_nav_foot":
+            return {
+                ...state,
+                show: state.show
+            }
+            
+        case "sign_in":
+            return {
+                ...state,
+                auth: true,
+                user: action.user
+            }
+
+        case "log_out":
+            return {
+                ...state,
+                auth: false,
+                user: null
+            }
+
+        default:
+            return state;
+    }
+}
+
+function GeneralProvider({children}){
+    let [state, dispatch] = useReducer(Reducer, {show: true, auth: false, user: null});
+
+    let data = {state, dispatch};
+
+    return(
+        <GeneralContext.Provider value={data}>
+            {children}
+        </GeneralContext.Provider>
+    );
+}
+
+export {GeneralProvider};
+export default GeneralContext;
